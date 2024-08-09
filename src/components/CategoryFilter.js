eb1701728f170b4eb019131f42adf404f2c98169
Clara Thuo <clarathuo0@gmail.com>
2024-08-09 05:03:12 +0300

    initial commit

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
-import React from "react";
 
-function CategoryFilter() {
+import React, { useState } from "react";
+
+function CategoryFilter({ categories }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map(category => (
+        <button 
+          key={category} 
+          className={category === selectedCategory ? "selected" : ""} 
+          onClick={() => handleCategoryClick(category)}
+        >
+          {category}
+        </button>
+      ))}
     </div>
   );
 }
